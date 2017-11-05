b3ccb38fc7020828f27ad56c98ffa84f9a89b2a4
Guister <guilarga@hotmail.com>
2017-11-05 16:52:36 +0000

    Test

---
+++ Source/BatteryCollector/BatteryPickup.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BatteryPickup.h"
+#include "Classes/Components/StaticMeshComponent.h"
+
+
+// Sets default values for this actor's properties
+ABatteryPickup::ABatteryPickup()
+{
+	GetMesh()->SetSimulatePhysics(true);
+}
+
+void ABatteryPickup::WasCollected_Implementation()
+{
+	Super::WasCollected_Implementation();
+	Destroy();
+}
